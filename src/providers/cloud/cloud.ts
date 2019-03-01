3310776cd2bb37630688a97c8b1173a47ada285e
mouhamed <annemouha95@gmail.com>
2019-03-01 15:13:39 +0000

    Now playing background design

--- src/providers/cloud/cloud.ts
+++ src/providers/cloud/cloud.ts
@@ -17,32 +17,11 @@ export class CloudProvider {
     { url: '../assets/audio/Booba-Madrina-Feat.-Maes.mp3',
       name: 'Maes ft. Booba'
     },
-    { url: '../assets/audio/Migos - BBO Bad Bitches Only.mp3',
-      name: 'BBO Bad Bitches Only'
-    },
     { url: '../assets/audio/Migos - Beast.mp3',
       name: ' Beast'
     },
-    { url: '../assets/audio/Migos - Flooded.mp3',
-      name: 'Flooded'
-    },
     { url: '../assets/audio/Migos - Gang Gang.mp3',
       name: 'Gang Gang'
-    },
-    { url: '../assets/audio/Migos - Higher We Go.mp3',
-      name: 'Higher We Go'
-    },
-    { url: '../assets/audio/Migos - Movin Too Fast.mp3',
-      name: ' Movin Too Fast'
-    },
-    { url: '../assets/audio/Migos - Narcos.mp3',
-      name: 'Narcos'
-    },
-    { url: '../assets/audio/Migos - Top Down On Da NAWF.mp3',
-      name: 'Top Down On Da NAWF'
-    },
-    { url: '../assets/audio/Migos - Walk It Talk It.mp3',
-      name: 'Walk It Talk It'
     }
   ];
   getFiles() {
